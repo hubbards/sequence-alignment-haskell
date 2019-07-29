fa2d28bc29610de99e9c6e95923473ed2eb3f5b7
Spencer Hubbard <hubbardspencerm@gmail.com>
2019-07-28 17:33:24 -0700

    Use applicative style

--- app/Main.hs
+++ app/Main.hs
@@ -7,12 +7,12 @@ import Align
 
 main :: IO ()
 main = do (f1 : f2 : _) <- getArgs
-          s1 <- fmap lines (readFile f1)
-          s2 <- fmap lines (readFile f2)
+          s1 <- lines <$> readFile f1
+          s2 <- lines <$> readFile f2
           let delta = 2
-              alpha = \ a b -> if a == b then -2 else 1
+              alpha a b = if a == b then -2 else 1
               f = ("Loaded: " ++) . show . head
-              g = \ x y -> show (run $ Prob delta alpha x y)
+              g x y = show (run $ Prob delta alpha x y)
               h = unlines . tail
           putStrLn (f s1)
           putStrLn (f s2)
