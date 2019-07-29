f0a19a05b5dfe8a93e786cd88d038109bfa8ab6d
Spencer Hubbard <hubbardspencerm@gmail.com>
2019-07-29 11:00:43 -0700

    Refactor main function

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,22 @@
 -- TODO: comment
 
-import Data.Function
 import System.Environment
 
 import Align
 
 main :: IO ()
-main = do (f1 : f2 : _) <- getArgs
-          s1 <- lines <$> readFile f1
-          s2 <- lines <$> readFile f2
-          let delta = 2
-              alpha a b = if a == b then -2 else 1
-              f = ("Loaded: " ++) . show . head
-              g x y = show (run $ Prob delta alpha x y)
-              h = unlines . tail
-          putStrLn (f s1)
-          putStrLn (f s2)
+main = do (file1 : file2 : _) <- getArgs
+          (title1 : seq1) <- lines <$> readFile file1
+          (title2 : seq2) <- lines <$> readFile file2
+          let seq1' = unlines seq1
+              seq2' = unlines seq2
+          putStrLn ("Loaded: " ++ title1)
+          putStrLn ("Loaded: " ++ title2)
           putStrLn "Alignment:"
-          putStr $ (g `on` h) s1 s2
+          print (run $ Prob delta alpha seq1' seq2')
+
+delta :: Cost
+delta = 2
+
+alpha :: Char -> Char -> Cost
+alpha a b = if a == b then -2 else 1
